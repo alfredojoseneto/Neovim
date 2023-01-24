2e01135d60c6da2df4206a7db9a3b0112162ffbd
Alfredo Neto <alfredojoseneto@gmail.com>
2023-01-24 06:04:34 -0300

    add: added plugins to install

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -75,10 +75,25 @@ return require('packer').startup(function(use)
   }
 
   -- LSP
-  use { 'neovim/nvim-lspconfig' }
+  -- LSP Support
+  use{
+    'williamboman/mason.nvim',
+    requires = {
+      {'neovim/nvim-lspconfig'},             -- Required
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},         -- Required
+      {'hrsh7th/cmp-nvim-lsp'},     -- Required
+      {'hrsh7th/cmp-buffer'},       -- Optional
+      {'hrsh7th/cmp-path'},         -- Optional
+      {'saadparwaiz1/cmp_luasnip'}, -- Optional
+      {'hrsh7th/cmp-nvim-lua'},     -- Optional
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},             -- Required
+    }
+  }
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  --
+
   -- Put this at the end after all plugins
   if packer_bootstrap then
     require('packer').sync()

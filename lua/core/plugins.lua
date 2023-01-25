0fad442336760a3e5738858b36af9fae4d6b8f09
Alfredo Neto <alfredojoseneto@gmail.com>
2023-01-25 09:48:39 -0300

    adicionado markdown

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -60,7 +60,21 @@ return require('packer').startup(function(use)
     config = function()
       require('gitsigns').setup()
     end
-}
+  }
+
+  -- Markdown
+  -- install without yarn or npm
+  use({
+      "iamcco/markdown-preview.nvim",
+      run = function()
+      vim.fn["mkdp#util#install"]() end,
+  })
+  use({ "iamcco/markdown-preview.nvim",
+    run = "cd app && npm install",
+    setup = function()
+      vim.g.mkdp_filetypes = { "markdown" } end,
+    ft = { "markdown" },
+  })
 
   -- Alpha Dashboard
   use { 'goolord/alpha-nvim',
